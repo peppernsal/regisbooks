1566a5ed15c6040d49afdcdd0a7f3764eaedec4e
Carl Furtado <carlzfurtado@gmail.com>
2025-04-03 19:08:34 -0400

    improve UI for add listing

--- projects/regisbooks/website/static/js/add-listing.js
+++ projects/regisbooks/website/static/js/add-listing.js
@@ -1,5 +1,14 @@
 const bookISBN = new URLSearchParams(location.search).get("book");
 
+const waitingMessage = document.getElementById("waiting-message");
+waitingMessage.textContent = `Loading book from ISBN ${bookISBN}`;
+
+const loaderAnimation = setInterval(() => {
+	if (waitingMessage.textContent.slice(-3) == "...") waitingMessage.textContent = waitingMessage.textContent.slice(0, -3);
+
+	waitingMessage.textContent+=".";
+}, 500);
+
 if (!bookISBN) location.href = "/enter-isbn";
 
 (async () => {
@@ -9,7 +18,15 @@ if (!bookISBN) location.href = "/enter-isbn";
 		document.getElementById("book-author").textContent = bookInfo.author;
 		document.getElementById("book-isbn").textContent = `ISBN: ${bookISBN}`;
 		document.getElementById("book-publishing-info").textContent = `Published by ${bookInfo.publisher}, ${bookInfo.publishDate}`;
-		document.getElementById("book-cover-img").src = bookInfo.coverImageURL;
+		
+		if (bookInfo.coverImageURL != "<no-url>") {
+			document.getElementById("book-cover-img").src = bookInfo.coverImageURL;
+		} else {
+			document.getElementById("book-cover-img").remove();
+		}
+
+		clearInterval(loaderAnimation);
+		waitingMessage.remove();
 	} catch { // book doesn't exist
 		alert("Invalid ISBN!");
 		location.href = "/enter-isbn";
@@ -62,7 +79,51 @@ function addPickupLocation() {
 	const inputElem = document.createElement("input");
 	
 	inputElem.type = "text";
-	inputElem.placeholder = "ex. Some City, XY; Regis High School; etc.";
+	inputElem.placeholder = "ex. Fairfield, CT";
+
+	// animate example text
+	setInterval(() => {
+		if (inputElem.value != "") return;
+
+		if (inputElem.placeholder == "ex. Fairfield, CT") {
+			const nextExample = "ex. Regis High School";
+
+			for (let i = 0; i < nextExample.length; i++) {
+				if (i < inputElem.placeholder.length) {
+					setTimeout(() => {
+						inputElem.placeholder = nextExample.slice(0, i+1)+inputElem.placeholder.slice(i+1);
+					}, 50 * i);
+				} else {
+					setTimeout(() => {
+						inputElem.placeholder+=nextExample[i];
+					}, 50 * i);
+				}
+			}
+
+			setTimeout(() => {
+				inputElem.placeholder = nextExample; // ensure set
+			}, 50 * nextExample.length);
+		} else {
+			const nextExample = "ex. Fairfield, CT";
+
+			for (let i = 0; i < nextExample.length; i++) {
+				if (i < inputElem.placeholder.length) {
+					setTimeout(() => {
+						inputElem.placeholder = nextExample.slice(0, i+1)+inputElem.placeholder.slice(i+1);
+					}, 50 * i);
+				} else {
+					setTimeout(() => {
+						inputElem.placeholder+=nextExample[i];
+					}, 50 * i);
+				}
+			}
+
+			setTimeout(() => {
+				inputElem.placeholder = nextExample; // ensure set
+			}, 50 * nextExample.length);
+		}
+	}, 4000);
+
 	inputElem.className = "form-control pickup-location";
 
 	inputDiv.appendChild(inputElem);

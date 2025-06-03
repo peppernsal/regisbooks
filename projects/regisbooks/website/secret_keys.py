afcc63228542e7cc6757d29af025bd2ebb052cbc
Carl Furtado <carlzfurtado@gmail.com>
2025-06-03 10:21:18 -0400

    add email whitelist

--- projects/regisbooks/website/secret_keys.py
+++ projects/regisbooks/website/secret_keys.py
@@ -10,3 +10,4 @@ AUTH_URL = os.getenv("REGISBOOKS_AUTH_URL")
 AUTH_API_KEY = os.getenv("REGISBOOKS_AUTH_API_KEY")
 ADMIN_KEY = os.getenv("ADMIN_KEY")
 DB_URI = os.getenv("DB_URI")
+EMAIL_WHITELIST = os.getenv("EMAIL_WHITELIST", "").split(",") if os.getenv("EMAIL_WHITELIST") else []

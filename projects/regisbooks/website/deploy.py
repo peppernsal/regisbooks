43ea2d52ed8eb7edb726335ef1b47675e98a158e
Carl Furtado <carlzfurtado@gmail.com>
2025-06-03 15:00:52 -0400

    add deployment code

---
+++ projects/regisbooks/website/deploy.py
@@ -0,0 +1,13 @@
+from waitress import serve
+from app import app, webpy_setup
+from json import load
+from webpy.fs_routes import parse_fs_routes
+
+with open("config.json", 'r') as f:
+	config = load(f)
+
+webpy_setup(app)
+parse_fs_routes(app, "root", {}, {})
+print("Setup complete, starting server...")
+
+serve(app, host=config["host"], port=config["port"])

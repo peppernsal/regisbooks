c71750b6c3dac9cb44fb276e8d2896c466597fa8
Carl Furtado <carlzfurtado@gmail.com>
2025-06-05 20:27:27 -0400

    ensure accordion buttons update hash on help page

--- projects/regisbooks/website/static/js/help.js
+++ projects/regisbooks/website/static/js/help.js
@@ -3,3 +3,10 @@ const focusText = location.hash.substring(1);
 if (focusText !== "view-listings") {
 	document.getElementById(focusText).querySelector("button").click();
 }
+
+const accordionButtons = document.querySelectorAll(".accordion-button");
+accordionButtons.forEach(button => {
+	button.addEventListener("click", () => {
+		location.hash = button.parentElement.id;
+	});
+});

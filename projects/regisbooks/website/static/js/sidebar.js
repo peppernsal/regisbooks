856ca14ecb08cf1af8ae5c9d5b63c69c9d077ee2
Carl Furtado <carlzfurtado@gmail.com>
2025-06-19 11:50:31 -0400

    re-center content on sidebar collapse/expand, fixes #9

--- projects/regisbooks/website/static/js/sidebar.js
+++ projects/regisbooks/website/static/js/sidebar.js
@@ -1,24 +1,30 @@
 function toggleSidebar() {
 	const sidebar = document.querySelector('.sidebar');
+	const content = document.querySelector('.content');
 
 	if (sidebar.classList.contains('collapsed')) {
 		sidebar.classList.remove('collapsed');
+		content.classList.remove('centered');
 		localStorage.setItem('sidebarState', 'expanded');
 		
 		
 	} else {
 		sidebar.classList.add('collapsed');
+		content.classList.add('centered');
 		localStorage.setItem('sidebarState', 'collapsed');
 	}
 }
 
 document.addEventListener('DOMContentLoaded', () => {
 	const sidebar = document.querySelector('.sidebar');
+	const content = document.querySelector('.content');
 	const savedState = localStorage.getItem('sidebarState');
 
 	if (savedState === 'collapsed') {
 		sidebar.classList.add('collapsed');
+		content.classList.add('centered');
 	} else {
 		sidebar.classList.remove('collapsed');
+		content.classList.remove('centered');
 	}
 });

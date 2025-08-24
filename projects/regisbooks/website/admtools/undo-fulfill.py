6f06e13894799b4ba7613fa90017efae76f6393f
Carl Furtado <carlzfurtado@gmail.com>
2025-08-24 17:01:42 -0400

    fix undo fulfill tool

--- projects/regisbooks/website/admtools/undo-fulfill.py
+++ projects/regisbooks/website/admtools/undo-fulfill.py
@@ -3,7 +3,6 @@ import httpx
 import constants
 
 listing = sys.argv[1]
-status = int(sys.argv[2])
 
 resp = httpx.post(f"{constants.SERVER_URL}/api/external/undo-fulfill", json={ "listingID": listing, "key": constants.ADMIN_KEY })
 
